cf8b6a547386fb045d4d7be6a41db1c068aa803b
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2019-12-10 05:07:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,5 +6,5 @@ def meal_choice (veg1, veg2, protein = "meat")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("B", "M", "T")
+puts meal_choice("B", "M", "T")
 meal_choice("B", "M")
